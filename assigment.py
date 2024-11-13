56e0f060040973e7f1d6e0929648b777648c1ebc
Asma0445 <asmaabdulaziza@gmail.com>
2024-11-13 22:58:22 +0500

    Add files via upload

---
+++ assigment.py
@@ -0,0 +1,14 @@
+#sum=a+b+c
+a=10
+b=20
+c=30
+print(a+b+c)
+
+deffrence=a-b-c
+print(a-b-c)
+
+product=a*b*c
+print(a*b*c)
+
+average=a+b+c/3
+print(a+b+c/3)
---
+++ part2.py
@@ -0,0 +1,24 @@
+#area of triangle=0.5*h*b
+h=20
+b=5
+area=0.5*20*5
+print("area of triagle=",0.5*h*b)
+
+
+#area of circle =3.14*r*r
+r=15
+area = 3.14*r*r
+print("area of circle=",3.14*r*r)
+
+
+#area of square=s*s
+s=25
+area=s*s
+print("area of square=",s*s) 
+
+
+#area of rectangle=l*w
+l=15
+w=10
+area=l*w
+print("area of rectangle=",l*w)
